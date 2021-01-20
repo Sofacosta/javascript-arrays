ec094369e0ad726487f67c4cbbc5d9cd8ce73598
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-01-20 09:37:25 -0600

    create a project to practice javascript arrays

---
+++ js/Notes.js
@@ -0,0 +1,32 @@
+const simpsons = ["Lisa", "bart", "Maggie"];
+const myParents = ["Humberto", "Patricia"];
+const bothArrays = simpsons.concat(myParents);
+const pets = ["rex", "floffy"];
+pets.push("mouse");
+pets.reverse();
+myParents[0];
+simpsons[0];
+simpsons.push("marge");
+
+simpsons[0];
+simpsons[0] = "marge";
+simpsons; //"marge", "bart", "Maggie"]
+
+myParents[1];
+simpsons.pop();
+simpsons.push("Homer");
+
+let test = "";
+test = "sofia";
+
+.indexOf()
+simpsons.indexOf("bart"); // 1
+
+.find()
+number = [1,2,0,4];
+number.find(falsey);
+simpsons.splice(0, 3,"homer"); // ["homer"]
+
+.reverse()
+.sort()
+number.sort(); // [0, 1, 2, 4]
---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// $(document).ready(function () {
+//   $("#fav-form").submit(function (event) {
+//     event.preventDefault();
+//     const input1 = $("#input1").val();
+//     const input2 = $("#input2").val();
+//     const input3 = $("#input3").val();
+
+//     let results = [input1, input2, input3];
+
+//     $("#result1").text(results[0]);
+//     $("#result2").text(results[1];
+//     $("#result3").text(results[2];
+//   });
+// });
+
+let names = ["Woo Jin", "Sofia", "Tien"];
+nanes.forEach(function (element) {
+  console.log(element);
+});
